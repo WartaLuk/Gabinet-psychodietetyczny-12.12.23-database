1e9a7838836ff7fda032b7278ef54af0cacc724a
Łukasz Wartałowicz <wartalowicz.lukasz.praca@gmail.com>
2023-12-17 16:58:36 +0100

    demoData.sql

---
+++ seeds/demoData.sql
@@ -0,0 +1,8 @@
+-- demoData.sql
+
+USE gabinet_psychodietetyczny;
+
+INSERT INTO posts (title, content, image_url) VALUES
+('Wprowadzenie do psychodietetyki', 'Treść wprowadzająca do tematu psychodietetyki...', 'image1.jpg'),
+('Zdrowe odżywianie', 'Artykuł o znaczeniu zdrowego odżywiania...', 'image2.jpg'),
+('Porady dla zdrowego stylu życia', 'Porady i wskazówki dotyczące zdrowego stylu życia...', 'image3.jpg');
